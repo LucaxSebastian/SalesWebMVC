eecdaf7c16ef54512eb61196df733c66049e7cb2
Lucas Sebastian <lucassebastian167@gmail.com>
2022-09-25 21:46:22 -0300

    CRUD Scaffolding

---
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesWebMvcContext : DbContext
+    {
+        public DbSet<Department> Department { get; set; }
+
+        public SalesWebMvcContext(DbContextOptions<SalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+    }
+}
